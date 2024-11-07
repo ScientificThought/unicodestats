7bf184ba098f46fd392f369267b0e8370cab0a13
leandrocoelhoj <leandrocoelhoj@gmail.com>
2024-11-07 00:02:03 -0300

    Creating the dict whit all characters in the txt document and printing it

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 file = open('data/dom-casmurro.txt', mode='r+', encoding='utf-8')
 
+characters = {}
+
 for line in file:
-    print(line)
+    lower_line = line.lower().replace('\n', '').replace(' ', '')
+    line_characters = list(lower_line)
+    for character in line_characters:
+        if character not in characters.keys():
+            characters[character] = 1
+
+        else:
+            characters[character] += 1
+
+print(characters)
 
