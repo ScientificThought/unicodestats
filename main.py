f6846ee4f14256a173650eb4003d2779be997c70
leandrocoelhoj <leandrocoelhoj@gmail.com>
2024-11-18 20:22:57 -0300

    Revert to eed42556

--- main.py
+++ main.py
@@ -1,36 +1,16 @@
-class Unicodestats:
-    @staticmethod
-    def open_file(filename):
-        """
-        Open the file and returns the contentt.
-        The "filename" needs to be inside "data" folder.
-        """
-        try:
-            with open(f'data/{filename}', mode='r', encoding='utf-8') as file:
-                return file.read()
-        except FileNotFoundError:
-            print(f"O arquivo {filename} não foi encontrado. Cheque se o arquivo realmente está dentro da pasta 'data'.")
-            return None
+file = open('data/dom-casmurro.txt', mode='r+', encoding='utf-8')
 
-    @staticmethod
-    def characters_count(text):
-        """
-        Counts the number of characters in the text, creating a histogram.
-        """
-        cleaned_text = text.lower().replace(' ', '').replace('\n', '')
+characters = {}
 
-        characters_dict = {}
+for line in file:
+    lower_line = line.lower().replace('\n', '').replace(' ', '')
+    line_characters = list(lower_line)
+    for character in line_characters:
+        if character not in characters.keys():
+            characters[character] = 1
 
-        for char in cleaned_text:
-            if char in characters_dict:
-                characters_dict[char] += 1
-            else:
-                characters_dict[char] = 1
+        else:
+            characters[character] += 1
 
-        return characters_dict
-
-if __name__ == "__main__":
-    file_content = Unicodestats.open_file('dom-casmurro.txt')
-    characters = Unicodestats.characters_count(file_content)
-    print(characters)
+print(characters)
 
