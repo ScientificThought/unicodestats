6f8331bfcbeaf1ae9f7ec903ca534ed20142f108
leandrocoelhoj <leandrocoelhoj@gmail.com>
2024-11-18 21:10:16 -0300

    Turn to function

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-file = open('data/dom-casmurro.txt', mode='r+', encoding='utf-8')
+def main():
+    file = open('data/dom-casmurro.txt', mode='r+', encoding='utf-8')
+    characters_dict = {}
+    for line in file:
+        lower_line = line.lower().strip()
+        characters = list(lower_line)
+        for character in characters:
+            n = characters_dict.get(character, 0) + 1
+            characters_dict[character] = n
+    print(characters_dict)
 
-characters_dict = {}
-
-for line in file:
-    lower_line = line.lower().strip()
-    line_characters = list(lower_line)
-    for character in line_characters:
-        n = characters_dict.get(character, 0) + 1
-        characters_dict[character] = n
-
-print(characters_dict)
+if __name__ == "__main__":
+    main()
